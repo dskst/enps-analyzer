11d2602d36c8e688168746065f4d3ff6025689fb
dskst <10830352+dskst@users.noreply.github.com>
2020-12-18 21:15:51 +0900

    First commit

---
+++ correlation.R
@@ -0,0 +1,27 @@
+library("scatterplot3d")
+
+data <- read.csv("data/input.csv", header=TRUE)
+
+# 1行目はeNPSとして扱う
+enps <- data[,1]
+
+# 2行目以降はスコアデータ
+scores <- data[2:length(data)]
+
+# スコア平均を算出
+avg_score <- apply(scores, 2, mean)
+
+# eNPSとスコアの相関を算出
+cor_enps <- cor(enps, scores)
+
+# 二次元散布図を作成
+plot(avg_score, cor_enps, type = "n")
+text(avg_score, cor_enps)
+
+# 相関係数の優位性を検定
+cor_tests <- vector(mode = "numeric",)
+for (score in scores) {
+  cor_test <- cor.test(enps, score)
+  cor_tests <- c(cor_tests, cor_test$p.value)
+}
+
