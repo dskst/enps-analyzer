181a58a7e95aa51b2e607aa1afd567d2584b25d0
dskst <10830352+dskst@users.noreply.github.com>
2020-12-19 17:02:40 +0900

    Add avg glid  at 3D plot

--- correlation.R
+++ correlation.R
@@ -4,21 +4,22 @@ library("rgl")
 
 data <- read.csv("data/input.csv", header=TRUE)
 
-# 1行目はeNPSとして扱う
+# 1列目はeNPSとして扱う
 enps <- data[,1]
 
-# 2行目以降はスコアデータ
+# 2列目以降はスコアデータ
 scores <- data[2:length(data)]
 
 # スコア平均を算出
-avg_score <- apply(scores, 2, mean)
+avg_scores <- apply(scores, 2, mean)
 
 # eNPSとスコアの相関を算出
 cor_enps <- cor(enps, scores)
 
 # 二次元散布図を作成
-#plot(avg_score, cor_enps, type = "n")
-#text(avg_score, cor_enps)
+plot(avg_scores, cor_enps, type = "n")
+abline(h = mean(cor_enps), v = mean(avg_scores), col="#dcdcdc")
+text(avg_scores, cor_enps)
 
 # 相関係数の優位性を検定
 cor_tests <- vector(mode = "numeric",)
@@ -27,5 +28,8 @@ for (score in scores) {
   cor_tests <- c(cor_tests, cor_test$p.value)
 }
 
-plot3d(avg_score, cor_tests, cor_enps, type = "n")
-text3d(avg_score, cor_tests, cor_enps, c(1:length(cor_enps)))
+# 3D散布図を作成する
+plot3d(avg_scores, cor_tests, cor_enps, type = "h", col = ifelse(cor_tests <= 0.05, "#d52b2b", "#ffd5d5"))
+text3d(avg_scores, cor_tests, cor_enps, c(1:length(cor_enps)))
+grid3d("x", at = list(x = mean(avg_scores)), col = "#dcdcdc") 
+grid3d("z", at = list(z = mean(cor_enps)), col = "#dcdcdc")
