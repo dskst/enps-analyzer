75f3341060256e7defb2be747a96c35939539bfb
dskst <10830352+dskst@users.noreply.github.com>
2023-03-31 01:01:20 +0900

    refactor: refactor grid view

--- correlation.py
+++ correlation.py
@@ -18,22 +18,33 @@ avg_scores = scores.mean()
 # eNPSとスコアの相関を算出
 cor_enps = scores.apply(lambda x: enps.corr(x), axis=0)
 
-# 二次元散布図を作成
-plt.scatter(avg_scores, cor_enps)
-plt.axhline(y=cor_enps.mean(), color="#dcdcdc")
-plt.axvline(x=avg_scores.mean(), color="#dcdcdc")
-plt.show()
-
 # 相関係数の優位性を検定
 cor_tests = []
 for col in scores:
     cor_test = stats.pearsonr(enps, scores[col])
     cor_tests.append(cor_test[1])
 
-# 3D散布図を作成する
+colors = np.where(np.array(cor_tests) <= 0.05, "#d52b2b", "#ffd5d5")
+
+# 二次元散布図を作成
+plt.scatter(avg_scores, cor_enps, c=colors)
+
+# ラベルをセット
+headers = list(scores.columns)
+for i, header in enumerate(headers):
+    plt.annotate(header, (avg_scores[i], cor_enps[i]))
+
+plt.axhline(y=cor_enps.mean(), color="#dcdcdc")
+plt.axvline(x=avg_scores.mean(), color="#dcdcdc")
+plt.xlabel("Average Scores")
+plt.ylabel("Correlation with eNPS")
+plt.title("2D Scatter Plot")
+plt.grid(False)
+plt.show()
+
+# 3D散布図を作成
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-colors = np.where(np.array(cor_tests) <= 0.05, "#d52b2b", "#ffd5d5")
 ax.scatter(avg_scores, cor_tests, cor_enps, c=colors, marker='o')
 ax.set_xlabel('Avg Scores')
 ax.set_ylabel('Cor Tests')
